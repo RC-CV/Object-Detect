965fd982c349211c40e3227543913a9a998ecf79
JackKoLing <30818910+JackKoLing@users.noreply.github.com>
2018-05-24 08:16:25 +0800

    Update ballDetect.py

--- objectTrack/ballDetect.py
+++ objectTrack/ballDetect.py
@@ -1,20 +1,25 @@
 # coding:utf8
 import cv2
 
-
 def detect_video(video):
     camera = cv2.VideoCapture(video)
     history = 20    # 训练帧数
 
-    bs = cv2.createBackgroundSubtractorKNN(detectShadows=True)  # 背景减除器，设置阴影检测
+    # bs = cv2.createBackgroundSubtractorKNN(detectShadows=True)  # 背景减除器，设置阴影检测
+    # bs = cv2.bgsegm.createBackgroundSubtractorMOG(history=history) # 效果不行
+    bs = cv2.createBackgroundSubtractorMOG2(history=history, detectShadows=True) #这个效果最好
+
     bs.setHistory(history)
+
+    ball_x = 0
+    ball_y = 0
 
     frames = 0
     points=[]
 
     while True:
         res, frame = camera.read()
-
+        frame = cv2.GaussianBlur(frame, (21, 21), 0) #用高斯滤波检测更好点
         if not res:
             break
 
@@ -31,25 +36,49 @@ def detect_video(video):
         # 获取所有检测框
         image, contours, hier = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+        # print(len(contours))
+        if(len(contours) > 4):
+            print("shock , no ball")
+            continue
         for c in contours:
             # 获取矩形框边界坐标
             x, y, w, h = cv2.boundingRect(c)
-            
+            # print([x,y,w,h])
             # 计算矩形框的面积
             area = cv2.contourArea(c)
-            print("Ball capture",area,w,h)
+            # print(area)
 
-            if 100 < area < 2000:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                points.append([x,y,w,h])
-                #print(area)
+            if 200 < area < 2500 :
+                # print([ball_x,ball_y])
+                if ball_x== 0 and ball_y ==0 and y < 200:
+                    ball_x = x
+                    ball_y = y
+                    print("First ball get",[ball_x ,ball_y])
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    points.append([x,y,w,h])
+                else:
+                    if (ball_x -100 < x < ball_x+100) and ( y < ball_y+240):
+                        ball_x = x
+                        ball_y = y
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                        print("Next ball",[ball_x ,ball_y])
+                        points.append([x,y,w,h])
+                    elif ball_x==0 and ball_y ==0  :
+                        print("no ball")
+                        break
+                    else:
+                        print("Scanner Moved!")
+                        ball_y += 50
+                        break
 
         cv2.imshow("detection", frame)
         cv2.imshow("back", dilated)
         if cv2.waitKey(110) & 0xff == 27:
+            # 按Esc退出
             break
-           
+
     camera.release()
+    cv2.destroyAllWindows()
     return points
 
 '''
